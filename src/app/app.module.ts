789d74c9643ec6267e0815225fbd10239748d2c9
Ganesh Nemade <ganesh.nemade@xoriant.com>
2018-03-01 09:30:28 +0530

    Added blank services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { SlidesRepositoryService } from './../services/slides-repository.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, InjectionToken } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+let SlidesRepository = new InjectionToken<string>("SlidesRepository"); 
+
 
 @NgModule({
   declarations: [
@@ -24,6 +28,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    SlidesRepositoryService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { BaseRepository } from './../../../app-models/slides-repository';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+    public slidesRepo: BaseRepository
+  ) {
+    // Sequence
+    console.log('--> In Constructor');
+  }
 
+  ionViewDidLoad(){
+   console.log( '--> In ionViewDidLoad', '');
+  }
+
+  ionViewWillEnter(){
+    console.log( '--> In ionViewWillEnter', '');
+  }
+
+  ionViewDidEnter(){
+    console.log( '--> In ionViewDidEnter', '');
+  }
+
+  ionViewWillLeave(){
+    console.log( '--> In ionViewWillLeave', '');
+  }
+
+  ionViewDidLeave(){
+    console.log( '--> In ionViewDidLeave', '');
+  }
+
+  ionViewWillUnload(){
+    console.log( '--> In ionViewWillUnload', '');
   }
 
 }
